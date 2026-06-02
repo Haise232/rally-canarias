9f232b0986dc3f0f11701135f290d4b9fd61784e
Haise232 <Joaquinjose1298@gmail.com>
2026-06-02 17:15:09 +0100

    Base del proyecto

---
+++ src/main/java/com/rally/canarias/CanariasApplication.java
@@ -0,0 +1,13 @@
+package com.rally.canarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanariasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rally/canarias/CanariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rally.canarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
