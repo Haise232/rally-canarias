89f505e174c55eee4b3037a1268ab8bf3ea2593e
haise232 <Joaquinjose1298@gmail.com>
2026-06-03 01:35:04 +0100

    feat: añadir métodos de búsqueda a PilotoRepository

---
+++ src/main/java/com/rally/canarias/repository/PilotoRepository.java
@@ -0,0 +1,21 @@
+package com.rally.canarias.repository;
+
+import com.rally.canarias.entity.Piloto;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PilotoRepository extends JpaRepository<Piloto, Long> {
+
+    List<Piloto> findByEquipoId(Long equipoId);
+
+    List<Piloto> findByNombreContainingIgnoreCase(String nombre, Sort sort);
+
+    @Query("SELECT p FROM Piloto p JOIN p.tramos t GROUP BY p HAVING COUNT(t) >= :minTramos")
+    List<Piloto> findPilotosConMinimoTramos(@Param("minTramos") int minTramos);
+}
